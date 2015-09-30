1e37f06cac643bf19435561d07404f80dd17acf0
Patrick Madden <maddenpj@gmail.com>
2015-09-30 10:28:47 -0500

    install zsh completions

--- zsh/env.sh
+++ zsh/env.sh
@@ -1,2 +1,3 @@
 
 # stuff
+alias  docker-connect="DOCKER_HOST=tcp://192.168.99.100:2376 DOCKER_CERT_PATH=/Users/patrick/.docker/machine/machines/default DOCKER_TLS_VERIFY=1 /usr/local/bin/zsh && docker info"
