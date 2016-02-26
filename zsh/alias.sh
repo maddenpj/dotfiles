1872919dd98e557af989997d4b19b4d498d4cf35
Patrick Madden <maddenpj@gmail.com>
2016-02-25 18:44:21 -0600

    Add some useful github alias

--- zsh/alias.sh
+++ zsh/alias.sh
@@ -6,4 +6,6 @@ alias l='showmarks'
 alias h='history'
 alias wget-rip='wget -mkEpnp -e robots=off'
 alias gs='git status'
+alias gc='git clone'
+alias github-push='git push origin master'
 function hs { history | grep -i $* }
