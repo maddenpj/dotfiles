162f63a3023dddf1aa000a7ee19fe376e1aa354d
Patrick Madden <maddenpj@gmail.com>
2017-02-28 17:59:25 -0600

    Random catchup

--- zsh/alias.sh
+++ zsh/alias.sh
@@ -1,11 +1,17 @@
 alias ltr='ls -ltr'
+alias lt='ls -lt'
 alias rsync-dir='rsync -avz --no-perms'
 alias reload='echo "Reloading zsh configuration"; source ~/.zshrc'
-alias j='jump'
-alias l='showmarks'
-alias h='history'
+# alias j='jump'
+# alias l='showmarks'
+# alias h='history'
 alias wget-rip='wget -mkEpnp -e robots=off'
 alias gs='git status'
 alias gc='git clone'
 alias github-push='git push origin master'
+
+alias bc='brew cask'
+
 function hs { history | grep -i $* }
+# alias gcc='/usr/local/bin/gcc-4.6'
+# alias g++='/usr/local/bin/g++-4.6'
