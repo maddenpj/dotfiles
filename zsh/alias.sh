a11afc7ea86b7af501c36bb72b047a3eb968f34d
Patrick Madden <maddenpj@gmail.com>
2018-03-31 22:26:26 -0500

    This is gonna break stuff I bet

--- zsh/alias.sh
+++ zsh/alias.sh
@@ -12,6 +12,14 @@ alias github-push='git push origin master'
 
 alias bc='brew cask'
 
+alias tmuxls='tmux list-sessions'
+alias tmuxa='tmux attach -t'
+
+
+function convert_slack_width  { convert $1 -resize 128x "${1:r}-i.${1:e}" }
+function convert_slack_height { convert $1 -resize x128 "${1:r}-i.${1:e}" }
 function hs { history | grep -i $* }
 # alias gcc='/usr/local/bin/gcc-4.6'
 # alias g++='/usr/local/bin/g++-4.6'
+
+alias gpg='gpg1'
